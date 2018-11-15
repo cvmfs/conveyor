b74f374965ff87b3268f0245f484d69c9f10b080
Radu Popescu <mail@radupopescu.net>
2018-11-15 15:39:22 +0100

    Update submit_many.sh

--- test/submit_many.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-for i in $(seq 1 10) ; do
-    ./src/cvmfs_job submit --path /ripgrep-0.$i.0 test-sw.hsf.org http://cvmfs-publisher-test.s3.cern.ch/ripgrep/ripgrep-0.$i.0-x86_64-unknown-linux-musl.tar.gz
-done
