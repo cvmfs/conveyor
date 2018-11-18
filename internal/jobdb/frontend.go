72dcf2a9a7be68f1303330c55a9b780a26018383
Radu Popescu <mail@radupopescu.net>
2018-11-18 18:25:59 +0100

    Job DB service frontend WIP

--- internal/jobdb/frontend.go
+++ internal/jobdb/frontend.go
@@ -9,18 +9,25 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var apiRoot = "/api/v1"
+
 func getHome(w http.ResponseWriter, h *http.Request) {
-	w.Write([]byte("You are in an open field on the west side of a white house with a boarded front door."))
+	r := fmt.Sprintf(
+		"You are in an open field on the west side" +
+			"of a white house with a boarded front door.\n")
+	w.Write([]byte(r))
 }
 
 func getJob(w http.ResponseWriter, h *http.Request) {
-	id := mux.Vars(h)["id"]
-	log.Info.Printf("get job: %s", id)
+	ids := mux.Vars(h)["id"]
+	full := h.URL.Query()["full"]
+	r := fmt.Sprintln("get job:", ids, " full:", full)
+	w.Write([]byte(r))
 }
 
 func getJobs(w http.ResponseWriter, h *http.Request) {
-	ids := h.URL.Query()["ids"]
-	log.Info.Printf("get jobs: %s", ids)
+	r := fmt.Sprintln("get jobs:")
+	w.Write([]byte(r))
 }
 
 func startFrontEnd(port int) error {
@@ -28,18 +35,18 @@ func startFrontEnd(port int) error {
 
 	var r *mux.Route
 	r = router.NewRoute()
-	r.Path("/")
+	r.Path(apiRoot + "/")
 	r.HandlerFunc(getHome)
 
 	r = router.NewRoute()
-	r.Path("/jobs/{id}")
+	r.Path(apiRoot + "/jobs/{id}")
 	r.Methods("GET")
+	r.Queries("full", "")
 	r.HandlerFunc(getJob)
 
 	r = router.NewRoute()
-	r.Path("/jobs")
+	r.Path(apiRoot + "/jobs")
 	r.Methods("GET")
-	r.Queries("ids", "")
 	r.HandlerFunc(getJobs)
 
 	srv := &http.Server{
