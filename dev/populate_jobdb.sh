d561618871d12996503932f291c584bcb4577805
Radu Popescu <mail@radupopescu.net>
2018-11-19 15:48:35 +0100

    Fix populate_jobdb.sh after schema update

--- dev/populate_jobdb.sh
+++ dev/populate_jobdb.sh
@@ -9,6 +9,6 @@ for i in $(seq 1 10) ; do
         --repo test-sw.hsf.org \
         --payload http://cvmfs-publisher-test.s3.cern.ch/ripgrep/ripgrep-0.$i.0-x86_64-unknown-linux-musl.tar.gz \
         --path /ripgrep-0.$i.0 | tail -1 | jq -r '.ID')
-    psql -h localhost -U postgres -d TestCVMFS -w \
-        -c "insert into jobs (ID,Repository,Payload,RepositoryPath,Status) values ('$id','$repo','$payload','$path','success');"
+    psql -h localhost -U postgres -d devcvmfs -w \
+        -c "insert into jobs (ID,Repository,Payload,RepositoryPath,Successful) values ('$id','$repo','$payload','$path',true);"
 done
