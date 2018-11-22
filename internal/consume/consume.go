269d22fb2f8ca39c0f4b17473428f6960b02bfba
Radu Popescu <mail@radupopescu.net>
2018-11-22 16:29:26 +0100

    Implementing transaction script support in job consumer

--- internal/consume/consume.go
+++ internal/consume/consume.go
@@ -1,8 +1,13 @@
 package consume
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
 	"encoding/json"
+	"io/ioutil"
 	"os"
+	"os/exec"
 	"path"
 
 	"github.com/cvmfs/cvmfs-publisher-tools/internal/job"
@@ -63,16 +68,7 @@ func Run(qcfg queue.Config, tempDir string) error {
 		log.Info.Println("Start publishing job:", desc.ID.String())
 
 		task := func() error {
-			targetDir := path.Join(
-				"/cvmfs", desc.Repository, desc.RepositoryPath)
-			if err := os.MkdirAll(targetDir, 0755); err != nil {
-				return errors.Wrap(err, "could not create target dir")
-			}
-			log.Info.Println("Downloading payload:", desc.Payload)
-			if err := getter.Get(targetDir, desc.Payload); err != nil {
-				return errors.Wrap(err, "could not download payload")
-			}
-			return nil
+			return processTransaction(&desc, tempDir)
 		}
 
 		if err := RunTransaction(desc, task); err != nil {
@@ -84,6 +80,81 @@ func Run(qcfg queue.Config, tempDir string) error {
 
 		j.Ack(false)
 		log.Info.Println("Finished publishing job:", desc.ID.String())
+	}
+
+	return nil
+}
+
+func processTransaction(desc *job.Unprocessed, tempDir string) error {
+	// Create target dir if needed
+	targetDir := path.Join(
+		"/cvmfs", desc.Repository, desc.RepositoryPath)
+	if err := os.MkdirAll(targetDir, 0755); err != nil {
+		return errors.Wrap(err, "could not create target dir")
+	}
+
+	// Download and unpack the payload, if given
+	log.Info.Println("Downloading payload:", desc.Payload)
+	if err := getter.Get(targetDir, desc.Payload); err != nil {
+		return errors.Wrap(err, "could not download payload")
+	}
+
+	// Run the transaction script, if specified
+	if desc.Script != "" {
+		needsUnpacking := desc.TransferScript
+		log.Info.Printf(
+			"Running transaction script: %v (needs unpacking: %v)\n",
+			desc.Script, needsUnpacking)
+
+		var scriptFile string
+		if needsUnpacking {
+			var err error
+			scriptFile, err = unpackScript(desc.Script, tempDir)
+			if err != nil {
+				return errors.Wrap(err, "unpacking transaction script failed")
+			}
+		} else {
+			scriptFile = desc.Script
+		}
+
+		err := runScript(scriptFile, desc.Repository, desc.RepositoryPath, desc.ScriptArgs)
+		if err != nil {
+			return errors.Wrap(err, "running transaction script failed")
+		}
+	}
+
+	return nil
+}
+
+func unpackScript(script string, tempDir string) (string, error) {
+	buf, err := base64.StdEncoding.DecodeString(script)
+	if err != nil {
+		return "", errors.Wrap(err, "base64 decoding failed")
+	}
+	rd := bytes.NewReader(buf)
+	gz, err := gzip.NewReader(rd)
+	if err != nil {
+		return "", errors.Wrap(err, "gzip reader construction failed")
+	}
+	rawbuf, err := ioutil.ReadAll(gz)
+	if err != nil {
+		return "", errors.Wrap(err, "decompression failed")
+	}
+	scriptFileName := path.Join(tempDir, "transaction.sh")
+	if err := ioutil.WriteFile(scriptFileName, rawbuf, 0755); err != nil {
+		return "", errors.Wrap(err, "writing to disk failed")
+	}
+
+	return scriptFileName, nil
+}
+
+func runScript(script string, repo string, repoPath string, args string) error {
+	cmd := exec.Command(script, repo, repoPath, args)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Dir = path.Join("/cvmfs", repoPath)
+	if err := cmd.Run(); err != nil {
+		return err
 	}
 
 	return nil
