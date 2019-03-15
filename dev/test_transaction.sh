9974b070aebed3115cf02051f206af1b41a716d2
Radu Popescu <mail@radupopescu.net>
2019-03-15 15:43:50 +0100

    More verbose example transaction script

--- dev/test_transaction.sh
+++ dev/test_transaction.sh
@@ -11,9 +11,28 @@ lease_path=$2
 # URL of the archive which will be unpacked
 archive=$3
 
+echo "Running CernVM-FS transaction"
+echo "  - Repository: $repository"
+echo "  - Lease path: $lease_path"
+echo "  - Archive:    $archive"
+
+echo "- Changing directory to the root of the repository"
+
 cd /cvmfs/$repository
+
+echo "- Creating leased path, if needed"
+
 mkdir -p $lease_path && cd $lease_path
+
+echo "- Downloading archive"
+
 curl -o payload.tar.gz $archive
-tar xf payload.tar.gz
-rm payload.tar.gz
+
+echo "- Unpacking archive"
+
+tar xfv payload.tar.gz
+
+echo "- Cleaning up"
+
+rm -v payload.tar.gz
 
